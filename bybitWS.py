0340c011d592242307e0f61fcc7cc3391988182f
Ashten <hamiltof@oregonstate.edu>
2022-07-09 14:20:34 -0500

    added some features

--- bybitWS.py
+++ bybitWS.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import time, sleep
 from pybit import inverse_perpetual, usdt_perpetual
 
 ws_inverseP = inverse_perpetual.WebSocket(
@@ -8,14 +8,24 @@ ws_inverseP = inverse_perpetual.WebSocket(
     domain="bybit"  # the default is "bybit"
 )
 
-session_unauth = usdt_perpetual.HTTP(
+ws_usdt_perpetual = usdt_perpetual.WebSocket(
+    test=False,
+    ping_interval=3000,  # the default is 30
+    ping_timeout=10,  # the default is 10
+    domain="bybit"  # the default is "bybit"
+)
+
+session_usdtP = usdt_perpetual.HTTP(
+    endpoint="https://api.bybit.com")
+
+session_inverseP = inverse_perpetual.HTTP(
     endpoint="https://api.bybit.com")
 
 
 def queryAllSymbols():
     symbol_list = []
     removed = ["BTCUSDT", "USDCUSDT"]
-    symbols = session_unauth.query_symbol()
+    symbols = session_usdtP.query_symbol()
     for item in symbols['result']:
         if item['name'] in removed:
             continue
@@ -45,8 +55,13 @@ def handle_liquidations(msg):
     print(msg)
 
 
-def index_weights():
-    return
+def index_beta():
+    symbol_list = queryAllSymbols()
+    previous_hour = int(time()) - (60 * 60)
+    print(symbol_list)
+
+    for item in symbol_list:
+        response = session_usdtP.query_kline(symbol=item, interval=5, limit=12, from_time=previous_hour)
 
 
 while True:
